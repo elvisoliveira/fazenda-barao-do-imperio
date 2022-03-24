54fd5c5c21d1842ebf3990d65ecf3fe5bb35cfee
Elvis Oliveira <elvis.olv@gmail.com>
2022-03-24 10:14:47 -0300

    Setup of DEV and PROD env. Sass plugin added

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,33 @@
+const path = require('path');
+
+const production = (process.env.ELEVENTY_MODE || 'development') === 'production';
+
 const Image = require('@11ty/eleventy-img')
-const svgContents = require("eleventy-plugin-svg-contents");
-const fs = require('fs');
-const dir = 'galery'
+
+const htmlOptions = {
+    collapseWhitespace: true,
+    decodeEntities: true,
+    removeComments: true,
+};
+
+const sassOptions = {
+    outDir: path.normalize(path.join(__dirname, './_site')),
+    outFileName: 'styles',
+    outPath: '/stylesheets/',
+    outputStyle: production ? 'compressed' : 'expanded',
+    sassIndexFile: 'styles.scss',
+    sassLocation: path.normalize(path.join(__dirname, './stylesheets/')),
+};
 
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addGlobalData('timestamp', Date.now());
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addNunjucksShortcode("galleryImage", function (src, alt, sizes) {
         let options = {
             widths: [300, 600],
             formats: ["avif", "jpeg"],
-            urlPath: `/${dir}/`,
-            outputDir: `./_site/${dir}/`
+            urlPath: "/galery/",
+            outputDir: "./_site/galery/"
         };
         Image(src, options);
         return Image.generateHTML(Image.statsSync(src, options), {
@@ -21,7 +38,17 @@ module.exports = function (eleventyConfig) {
         });
     });
     eleventyConfig.addNunjucksGlobal("galleryFiles", function () {
+        const fs = require('fs');
         return fs.readdirSync('gallery');;
     });
-    eleventyConfig.addPlugin(svgContents);
+    eleventyConfig.addPlugin(require("eleventy-plugin-svg-contents"));
+    eleventyConfig.addPlugin(require('eleventy-plugin-dart-sass'), sassOptions);
+    if (production) {
+        const html = require('html-minifier');
+        eleventyConfig.addTransform('html', function (content, path) {
+            return path.endsWith('.html')
+                ? html.minify(content, htmlOptions)
+                : content;
+        });
+    }
 };
